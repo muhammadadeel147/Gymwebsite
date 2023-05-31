9dfdafd30b5fae076ef8e1858280336c8b0b91fb
Muhammad Adeel <muhammaddadeel90@gmail.com>
2023-06-01 01:08:35 +0500

    all changes done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,17 +6,18 @@ import hero_image_back from "../../src/assets/hero_image_back.png";
 import Heart from "../../src/assets/heart.png";
 import Calories from "../../src/assets/calories.png"
 import {motion} from "framer-motion"
-
+import NumberCounter from "number-counter"
 const Hero = () => {
     const transition ={type:"spring", duration:3}
+    const mobile=window.innerWidth<=768?true:false;
   return (
-  <div className="hero">
+  <div className="hero" id='home'>
     <div className="blur hero-blur"></div>
 <div className="left-h">
 <Header/>
 <div className="the-best-add">
     <motion.div
-    initial={{left:"238px"}}
+    initial={{left:mobile?"165px":"238px"}}
     whileInView={{left:"8px"}}
     transition={{...transition,type:"tween"}}
     
@@ -44,15 +45,17 @@ const Hero = () => {
 
 <div className="figures">
     <div>
-        <span>+140</span>
+        <span>
+            <NumberCounter end={140} start={100} delay="4" preFix="+"></NumberCounter>
+        </span>
         <span>expert coaches</span>
     </div>
     <div>
-        <span>+978</span>
+        <span> <NumberCounter end={978} start={800} delay="4" preFix="+"></NumberCounter></span>
         <span>members joined</span>
     </div>
     <div>
-        <span>+50</span>
+        <span> <NumberCounter end={50} start={0} delay="4" preFix="+"></NumberCounter></span>
         <span>fitness programs</span>
     </div>
 </div>
--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -6,7 +6,7 @@ const Programs = () => {
   return (
     <div className="Programs" id="programs">
 {/* header */}
-<div className="programs-header">
+<div className="programs-header" id='programs'>
     <span className='stroke-text'>Explore our </span>
     <span>Programs</span>
     <span className='stroke-text'>to shape you</span>
